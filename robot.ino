6e0eda2da8e2a25271f423f05ba69d73808d86f8
AayushPanda <aayush.vinayak@gmail.com>
2023-03-05 09:28:04 -0500

    add motor code

---
+++ robot.ino
@@ -0,0 +1,36 @@
+// Run a two wheeled robot using an h-bridge motor driver
+
+class Motor {
+    private:
+        uint8_t pin1;
+        uint8_t pin2;
+        uint8_t speedPin;
+
+    public:
+        int speed;
+        int direction;
+        Motor(uint8_t pin1, uint8_t pin2, uint8_t speedPin){
+            this->pin1 = pin1;
+            this->pin2 = pin2;
+            this->speedPin = speedPin;
+        }
+        void setSpeed(int speed){
+            this->speed = speed;
+            if (speed > 0){
+                digitalWrite(pin1, 1);
+                digitalWrite(pin2, 0);
+                analogWrite(speedPin, speed);
+            } else if (speed < 0){
+                digitalWrite(pin1, 0);
+                digitalWrite(pin2, 1);
+                analogWrite(speedPin, speed);
+            } else {
+                digitalWrite(pin1, 0);
+                digitalWrite(pin2, 0);
+                analogWrite(speedPin, 0);
+            }
+       }
+};
+
+Motor leftMotor(7, 8, 9);
+Motor rightMotor(12, 13, 14);
